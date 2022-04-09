5c5bc74a15f2d492321184c5a7b2e6aa24cf5f67
SyMind <dacongsama@live.com>
2022-04-09 12:45:22 +0800

    fix template babel config

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,11 +1,9 @@
-const path = require('path')
-
 module.exports = {
     presets: ['next/babel'],
     plugins: [
-        path.resolve('tarojs-plugin-platform-nextjs/babel/next-app-plugin'),
-        path.resolve('tarojs-plugin-platform-nextjs/babel/taro-app-plugin'),
-        path.resolve('tarojs-plugin-platform-nextjs/babel/taro-import-plugin'),
-        path.resolve('tarojs-plugin-platform-nextjs/babel/class-component-taro-router-plugin')
+        require.resolve('tarojs-plugin-platform-nextjs/babel/next-app-plugin'),
+        require.resolve('tarojs-plugin-platform-nextjs/babel/taro-app-plugin'),
+        require.resolve('tarojs-plugin-platform-nextjs/babel/taro-import-plugin'),
+        require.resolve('tarojs-plugin-platform-nextjs/babel/class-component-taro-router-plugin')
     ]
 }
