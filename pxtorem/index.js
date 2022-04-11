f049f09590efda5109531aef622fff5fdcd28ddb
SyMind <dacongsama@live.com>
2022-04-11 09:15:35 +0800

    postcss-pxtorem exclude node_modules

--- pxtorem/index.js
+++ pxtorem/index.js
@@ -8,7 +8,8 @@ const withPXToREM = (designWidth = 750) => config => {
         plugins: [
             [require.resolve('postcss-pxtorem'), {
                 rootValue: BASE_FONT_SIZE * designWidth / 640,
-                propWhiteList: []
+                propWhiteList: [],
+                exclude: /node_modules/
             }],
             ...plugins
         ]
