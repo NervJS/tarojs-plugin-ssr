8e07a2dfe569c4d46ca7e0fe186540926b699af8
SyMind <dacongsama@live.com>
2022-06-25 20:10:57 +0800

    refactor canvasToTempFilePath function

--- packages/nextjs/taro/src/canvas/canvasPutImageData.ts
+++ packages/nextjs/taro/src/canvas/canvasPutImageData.ts
@@ -48,6 +48,5 @@ const canvasPutImageDataInternal: typeof swan.canvasPutImageData = ({
 
 /**
  * 将像素数据绘制到画布。在自定义组件下，第二个参数传入自定义组件实例 this，以操作组件内 <canvas> 组件
- * @todo 暂未支持尺寸相关功能
  */
 export const canvasPutImageData = promisify(canvasPutImageDataInternal)
--- packages/nextjs/taro/src/canvas/canvasToTempFilePath.ts
+++ packages/nextjs/taro/src/canvas/canvasToTempFilePath.ts
@@ -1,23 +1,50 @@
-import { findDOM } from '../utils'
-import { MethodHandler } from '../utils/handler'
+import promisify from 'mpromisify'
+import type * as swan from '../swan'
+
+const canvasToTempFilePathInternal: typeof swan.canvasToTempFilePath = ({
+    canvasId,
+    fileType,
+    quality,
+    success,
+    fail,
+    complete
+}) => {
+    if (typeof window === 'undefined') {
+        throw new Error('`canvasPutImageData` does nothing on the server-side.')
+    }
+
+    if (typeof canvasId !== 'string') {
+        fail?.({
+            errMsg: `The canvasId param is required.`
+        })
+        complete?.()
+        return
+    }
+
+    const canvas = document.querySelector(`canvas[canvas-id="${canvasId}"]`) as HTMLCanvasElement
+    if (!canvas) {
+        fail?.({
+            errMsg: `Cannot find canvas by using canvasId: ${canvasId}.`
+        })
+        complete?.()
+        return
+    }
+
+    try {
+        const dataURL = canvas.toDataURL(`image/${fileType || 'png'}`, quality)
+        success?.({
+            tempFilePath: dataURL
+        })
+    } catch (err) {
+        fail?.({
+            errMsg: err.message
+        })
+    }
+    complete?.()
+}
 
 /**
  * 把当前画布指定区域的内容导出生成指定大小的图片。在 draw() 回调里调用该方法才能保证图片导出成功。
  * @todo 暂未支持尺寸相关功能
  */
-export const canvasToTempFilePath = ({ canvasId, fileType, quality, success, fail, complete }, inst) => {
-    const handle = new MethodHandler({ name: 'canvasToTempFilePath', success, fail, complete })
-    const el = findDOM(inst) as HTMLElement
-    const canvas = el?.querySelector(`canvas[canvas-id="${canvasId}"]`) as HTMLCanvasElement
-
-    try {
-        const dataURL = canvas?.toDataURL(`image/${fileType || 'png'}`, quality)
-        return handle.success({
-            tempFilePath: dataURL
-        })
-    } catch (e) {
-        return handle.fail({
-            errMsg: (e as any).message
-        })
-    }
-}
+export const canvasToTempFilePath = promisify(canvasToTempFilePathInternal)
