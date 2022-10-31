14d6d5934cad8e2c5f77de2f57c1197a1edc04e0
SyMind <dacongsama@live.com>
2022-10-31 22:44:21 +0800

    update with less demo

--- examples/with-less/src/pages/index/view.tsx
+++ examples/with-less/src/pages/index/view.tsx
@@ -2,9 +2,10 @@ import {View} from '@tarojs/components'
 import styles from './index.module.less';
 
 const IndexView = () => (
-    <View className={styles.main}>
-        Hello, world!
-    </View>
+    <>
+        <View className={styles.a}>混合（Mixins）</View>
+        <View className={styles.functions}>函数（Functions）</View>
+    </>
 );
 
 export default IndexView
